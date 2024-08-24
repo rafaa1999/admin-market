03dffe07f56558f1b83574ac42b0ca73941adf1f
rafaa1999 <raferhouma10@gmail.com>
2024-08-24 20:46:13 +0100

    Manage cart view modal

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartsService } from '../../services/carts.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProductsService } from 'src/app/products/services/products.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CartComponent implements OnInit {
   carts: any;
   form!: FormGroup;
+  products: any[] = [];
+  details: any;
+  total = 0;
 
-  constructor(private service: CartsService, private build: FormBuilder) {}
+  constructor(private service: CartsService, 
+    private build: FormBuilder,
+    private productService: ProductsService) { }
 
   ngOnInit(): void {
     this.form = this.build.group({
@@ -46,10 +52,21 @@ export class CartComponent implements OnInit {
     );
   }
 
-  deleteCart(id:number) {
-    this.service.deleteCart(id).subscribe(res => {
-      this.getAllCarts()
-      alert("Cart deleted Success")
-    })
+  deleteCart(id: number) {
+    this.service.deleteCart(id).subscribe((res) => {
+      this.getAllCarts();
+      alert('Cart deleted Success');
+    });
   }
+
+  view(index:number) {
+    this.products = []
+    this.details = this.carts[index];
+    for(let x in this.details.products) {
+      this.productService.getProductById(this.details.products[x].productId).subscribe(res => {
+        this.products.push({item: res , quantity:this.details.products[x].quantity})
+      })
+    }
+    console.log(this.details)
+  } 
 }
